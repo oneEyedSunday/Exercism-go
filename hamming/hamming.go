22d50ec4cea53ab8e9c6113ea7dbdae949928ac7
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-09-27 17:58:42 +0100

    Fit solution into Distance func

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -4,22 +4,18 @@ import (
 	"errors"
 )
 
-func getMismatched(first, second string) int {
-	mismatches := 0
-	for index := range first {
-		if first[index] != second[index] {
-			mismatches++
-		}
-	}
-
-	return mismatches
-}
-
 // Distance calculates the hamming distance or returns an error
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, errors.New("String lengths do not match")
 	}
 
-	return getMismatched(a, b), nil
+	mismatches := 0
+	for index := range a {
+		if a[index] != b[index] {
+			mismatches++
+		}
+	}
+
+	return mismatches, nil
 }
